1d6c358dadbd8a297df72f2db1171955ca93db10
Nuwandi-Dulshara <dulshar-ct19047@stu.kln.ac.lk>
2024-08-10 07:28:35 +0530

    add new

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,34 +1,46 @@
-import React, {useState,useEffect}  from "react";
+import React, {useState,useEffect}  from "react"; /*React: This is the core library for building user interfaces 
+in React.
+useState: This hook is used to manage the state within the functional component.
+useEffect: This hook is used to perform side effects, such as setting up an interval to update the time.*/
 
 function DigitalClock(){
 
-    const[time, setTime] = useState(new Date());
+    const[time, setTime] = useState(new Date()); /*time: This is a state variable that stores the current date and 
+    time.
+    setTime: This function is used to update the time state.useState(new Date()): Initializes the time state with 
+    the current 
+    date and time when the component is first rendered.*/
     useEffect(() => {
-        const interval_Id = setInterval(() => {
-            setTime(new Date());
-        },1000);
+        const interval_Id = setInterval(() => { /*This function is called inside the useEffect
+        hook to update the time state every second (1000 milliseconds).*/
+            setTime(new Date()); //update the current time every second
+        },1000); //(1000 milliseconds)
 
         return () => {
-            clearInterval(interval_Id);
+            clearInterval(interval_Id);/*`tops the timer when the component 
+            is unmounted, preventing memory leaks.*/
         }
     },[]);
 
-    function formatTime() {
-        let hrs = time.getHours();
+    function formatTime() { /*This function formats the current time into a 12-hour clock 
+        format with AM/PM notation.*/
+        let hrs = time.getHours(); /*getHours(), getMinutes(), getSeconds(): Methods used to extract hours,
+         minutes, and seconds from the time object.*/
         const mins = time.getMinutes();
         const secs = time.getSeconds();
-        const mer = hrs >= 12 ? "PM" : "AM";
+        const mer = hrs >= 12 ? "PM" : "AM"; 
     
-        hrs = hrs % 12 || 12;
+        hrs = hrs % 12 || 12; //hrs % 12 || 12: Converts the 24-hour time format to 12-hour format
     
-        return `${padZero(hrs)}:${padZero(mins)}:${padZero(secs)} ${mer}`;
+        return `${padZero(hrs)}:${padZero(mins)}:${padZero(secs)} ${mer}`; /*A helper function that adds a 
+        leading zero to numbers less than 10 for consistent two-digit display.*/
     }
     
     function padZero(number){
         return (number < 10 ? "0" : "") + number;
 
     }
-    return (
+    return ( //This JSX structure defines how the digital clock is displayed on the page.
        <div className="clock-c">
         <div className="clock"> 
         <span> {formatTime()}</span>
@@ -37,4 +49,4 @@ function DigitalClock(){
        </div>
     );
 }
-export default DigitalClock;
+export default DigitalClock; // Exporting the Component
